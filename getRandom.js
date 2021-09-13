5d36e994b6544aae26dd8cb54e23c1504c1836b3
sooowanlee <lsy9406@gmail.com>
2021-09-13 22:28:40 +0900

    import 공부전 getRandom,getType js파일 추가

---
+++ getRandom.js
@@ -0,0 +1,5 @@
+'use strict'
+
+export default function random() {
+  return Math.floor(Math.random() * 10);
+}
--- import.js
+++ import.js
@@ -1,1 +1,10 @@
 'use strict'
+
+import _ from 'lodash'; // From `node_modules`!
+import getType from './getType' // getType.js
+import getRandom from './getRandom'; // getRandom.js
+
+console.log(_.camelCase('the hello world'));
+console.log(getType([1, 2, 3]));
+console.log(getRandom(), getRandom());
+
