ae6034507519a2b6983f078a9567d0f6c4602a55
sooowanlee <lsy9406@gmail.com>
2021-09-14 21:16:09 +0900

    improt 공부

--- getRandom.js
+++ getRandom.js
@@ -1,5 +1,10 @@
-'use strict'
-
-export default function random() {
+export  function random() {
   return Math.floor(Math.random() * 10);
 }
+
+export const user = {
+  name: 'Soowan',
+  age: 28
+}
+
+export default 123;
--- import.js
+++ import.js
@@ -1,10 +1,15 @@
 'use strict'
-
+// 하나의 모듈에서는 하나의 exprot default만 가능 
+// name이 있는 export는 갯수 상관X
 import _ from 'lodash'; // From `node_modules`!
 import getType from './getType' // getType.js
-import getRandom from './getRandom'; // getRandom.js
+// 이름이있는 export 가져오는 법 (as 키워드는 사용해서 이름 변경 가능)
+// import { random, user as soowan} from './getRandom'; // getRandom.js
+import * as R from './getRandom';
 
 console.log(_.camelCase('the hello world'));
 console.log(getType([1, 2, 3]));
-console.log(getRandom(), getRandom());
+// console.log(random(), random());
+// console.log(soowan);
+console.log(R);
 
